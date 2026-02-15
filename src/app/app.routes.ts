a4180429c518336a145b7855d9530ed1e9f06194
bigfoot1144 <cole.sutyak@gmail.com>
2026-02-15 13:15:50 -0500

    make blog content semi-real

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { BioComponent } from './bio/bio.component';
 import { BlogIndexComponent } from './blog/blog-index.component';
-import { BLOG_POSTS } from './blog/blog-posts';
+import { MarkdownPostComponent } from './blog/markdown-post.component';
 
 export const routes: Routes = [
   { path: '', component: BioComponent },
   { path: 'blog', component: BlogIndexComponent },
-  ...BLOG_POSTS.map((post) => ({
-    path: `blog/${post.slug}`,
-    component: post.component
-  })),
+  { path: 'blog/:slug', component: MarkdownPostComponent },
   { path: '**', redirectTo: '' }
 ];
